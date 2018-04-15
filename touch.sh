612736cec9d01c6c4e039cdbd4e71ef6328b9905
takagotch <dyaccb@gmail.com>
2018-04-16 05:29:03 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ touch.sh
@@ -0,0 +1,27 @@
+//touch
+
+/*
+  -d time
+  -t time
+*/
+
+tky@DESKTOP-BUM395L MINGW64 
+
+ls -l test.text
+date
+touch test.text
+ls -l test.text
+
+ls -l hoge
+touch -d '2018-04-16 05:28' hoge
+ls -l hoge
+
+
+
+----
+
+
+
+
+
+
