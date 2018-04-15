485742ef39a89132393842afdede2a7e8cb0e5c0
takagotch <dyaccb@gmail.com>
2018-04-16 03:40:39 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ zip.sh
@@ -0,0 +1,19 @@
+
+zip arch.zip -r .
+
+zip arch.zip file*
+
+zip arch.zip file02.txt file03.txt -r .
+
+find . -type f | zip -@ arch.zip
+zip arch.zip -c file0*
+unzip -l arch.zip
+
+zip -z arch.zip
+unzip -l arch.zip
+
+zip arch.zip -e *.txt
+unzip arch.zip
+
+
+
