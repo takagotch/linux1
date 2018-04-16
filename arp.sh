9395797e11fe261368494f9f0eb28f0588fe8467
takagotch <dyaccb@gmail.com>
2018-04-17 07:57:27 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- arp.sh
+++ arp.sh
@@ -1,1 +1,19 @@
 //arp
+//netstat
+
+/**/
+
+arp
+arp -a
+arp -an
+arp jaguar
+
+
+
+arp 
+sudo arp -d gwrt
+arp
+
+
+---
+
--- host.sh
+++ host.sh
@@ -1,1 +1,19 @@
 //host,dig
+//
+
+/**/
+
+host www.google.co.jp
+host -t MX google.co.jp
+
+
+
+dig google.co.jp
+dig google.co.jp NS
+
+
+
+
+
+---
+
