287c0403eca7b69928cb9e0c0d0c043231de2685
takagotch <dyaccb@gmail.com>
2018-04-17 04:13:03 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- whereis.sh
+++ whereis.sh
@@ -1,10 +1,14 @@
 //whereis
-//
+//which
 
 
 /**/
 
 
+whereis cp
+whereis -b cp
+whereis -b -B /usr/bin cp
+whereis -b -B /usr/bin /bin -f cp
 
 
 
--- which.sh
+++ which.sh
@@ -1,1 +1,21 @@
 //which
+//whereis
+
+/**/
+
+env | grep PATH
+whereis which
+which which
+wchich -a which
+export PATH=/bin:$(PATH)
+env | grep PATH
+whereis which
+wchich which
+
+
+
+
+
+
+---
+
