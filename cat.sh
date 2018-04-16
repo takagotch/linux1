faa5f259041e02edf1c9a17d0de3f3839a552041
takagotch <dyaccb@gmail.com>
2018-04-16 19:44:22 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- cat.sh
+++ cat.sh
@@ -9,6 +9,8 @@
 -A
 */
 
+
+
 cat cat.sh
 cat cat1.sh
 cat cat.sh cat1.sh
@@ -18,6 +20,13 @@ cat -e cat.sh cat1.sh
 cat -t cat.sh cat1.sh
 
 
+ls -l
+ls -l | cat
+ls -l | cat -v
+rm "^H"
+ls -l | cat -e
+rm "^H "
+ls -l | cat -e
 
 ---
 
