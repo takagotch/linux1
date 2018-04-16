257daf7d483722c9a3bbbd722f6e9a5e22ee9568
takagotch <dyaccb@gmail.com>
2018-04-17 03:35:40 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- tr.sh
+++ tr.sh
@@ -1,1 +1,26 @@
 //tr
+
+/**/
+
+grep root /etc/group
+grep root /etc/group | tr ':' ','
+
+grep tky /etc/passwd
+grep tky /etc/password | tr -d ','
+
+
+grep tky /etc/passwd
+grep tky /etc/passwd | tr '[:lower:]' '[:upper]'
+
+
+
+
+
+
+
+
+---
+
+
+
+
