abc95df84cc1d08ad281888ef0a23c2f7e7c17a1
takagotch <dyaccb@gmail.com>
2018-04-17 03:45:39 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- find.sh
+++ find.sh
@@ -1,1 +1,18 @@
 //find
+//permission access
+//regexp
+
+/**/
+
+find /boot -name kernel.img -print
+find /tmp -atime +90 -exec rm () \:
+find /usr/include -name \*.h -print
+find /etc -type f -and -name \*conf\* -print
+find . \( -name \*.text -or -type d \) -print
+
+
+
+
+
+---
+
