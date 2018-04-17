2ed6d5e024ce12c336d6268f6c8c118149bf196c
takagotch <dyaccb@gmail.com>
2018-04-18 01:49:24 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- ln.sh
+++ ln.sh
@@ -1,1 +1,36 @@
 //ln
+//diff
+//ls
+
+
+/**/
+
+ls -l progre.text
+cat progre.text
+ln progre.text progre.list
+ls -l
+diff progre.text progre.list
+ln progre.text progre.list
+ln -i progre.text progre.list
+no
+rm progre.text
+ls -l
+cat progre.list
+
+
+
+ls -l
+ln -s progre.list progre.text
+ls -l
+cat progre.text
+rm progre.list
+ls -l
+cat progre.text
+
+
+
+
+
+
+---
+
