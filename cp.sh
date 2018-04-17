b0783cffa054d18ab61fbe26e7973c465e440a73
takagotch <dyaccb@gmail.com>
2018-04-18 01:33:47 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- cp.sh
+++ cp.sh
@@ -1,1 +1,44 @@
 //cp
+//alias
+
+/**/
+
+pwd
+ls -l ~tky/test.text
+cp ~tky/test.text
+cp ~tky/test.text
+ls -l test.text
+
+
+cp -i ~tky/test.text
+ls -l test.text
+cp ~tky/test.text
+ls -l test.text
+cp -p ~tky/test.text
+ls -l tky.test
+
+
+ls /root
+cp boot.log messages /root
+ls /root
+
+
+ls -CF
+ls -R testtree1
+cp -r testree1 testtree2
+ls -R testtree2
+cp -r testtree1/testdir1/testdir2/test1.text
+ls -CF
+mkdir testtree4
+cp -r testtree1/testdir1/testdir2/test1.text
+ls -R testtree4
+mkdir testtree5
+cp --parents testtree1/testdir1/testdir2/test1.text
+ls -R testtree5
+
+
+
+
+
+---
+
