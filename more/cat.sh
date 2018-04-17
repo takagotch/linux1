5adba2334725e7aa2e5ee1120a4e64f1d5399ba9
takagotch <dyaccb@gmail.com>
2018-04-18 08:59:34 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- more/cat.sh
+++ more/cat.sh
@@ -1,1 +1,24 @@
 //cat
+//tac
+
+
+/**/
+
+cat /usr/share/doc/coreutils/AUTHORS | head -n S
+
+cat -n /usr/share/doc/coreutils/AUTHOR | head -n 5
+
+zcat /usr/share/doc/coreutils/README.gz | \
+	cat - /usr/share/doc/coreutils/README.Debian \
+	> /tmp/READMEs
+
+cat - << EOF > /tmp/hoge
+cat /tmp/hoge
+
+
+cat a.txt
+cat b.txt
+echo "----" | cat a.txt - b.txt
+
+
+
