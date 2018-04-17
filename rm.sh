d4df12d08be69acbe14e0777f9afe3c047dfae49
takagotch <dyaccb@gmail.com>
2018-04-18 01:39:56 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- rm.sh
+++ rm.sh
@@ -1,1 +1,36 @@
 //rm
+//mkdir
+
+/**/
+
+ls -l
+rm test.text
+ls -l test.text
+rm readonly.text
+ls -l readonly.text
+
+
+
+ls -l readonly.text
+rm readonly.text
+ls -l readonly.text
+rm -f readonly.text
+ls -l readonly.text
+
+
+ls -R testdir
+rm testdir
+rm -r testdir
+ls -R testdir
+
+
+
+
+
+
+
+
+
+
+---
+
