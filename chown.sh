fc931cd446d15cbad6205eef472681c7d26930a8
takagotch <dyaccb@gmail.com>
2018-04-18 02:13:44 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- chown.sh
+++ chown.sh
@@ -1,1 +1,21 @@
 //chown
+//ls
+
+/**/
+
+ls -l
+chown tky:yes test.text
+sudo chown tky:yes test.text
+ls -l
+
+
+ls -ld subdir
+ls -l subdir
+sudo chown -R tky:yes subdir
+ls -l subdir
+
+
+
+
+---
+
