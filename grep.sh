fc963287986bdc6315011a07e5fb2f1d899f4439
takagotch <dyaccb@gmail.com>
2018-04-18 02:48:11 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- grep.sh
+++ grep.sh
@@ -1,1 +1,17 @@
 //grep,fgrep
+//regexp
+
+
+/**/
+
+cat /etc/shells
+grep tcsh /etc/shells
+grep -v sh /etc/shells
+
+
+grep ^/bin /etc/shels
+fgrep ^/bin /etc/shells
+
+
+---
+
--- sort.sh
+++ sort.sh
@@ -1,1 +1,22 @@
 //sort
+//uniq
+
+/**/
+
+%sort -t ',' filename
+
+%sort -t ':' -k 3n /etc/passwd
+%sort -t ':' -3 -n /etc/passwd
+
+
+
+cat progre.dat
+sort progre.dat
+
+sort -r progre.dat
+sort -t ',' -k 2 progre.dat
+
+
+
+---
+
