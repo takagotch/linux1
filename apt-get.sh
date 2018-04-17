85403514b88b0bd6bdf34443257cbddfc53dfccf
takagotch <dyaccb@gmail.com>
2018-04-18 07:26:51 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- apt-get.sh
+++ apt-get.sh
@@ -1,1 +1,17 @@
 //apt-get 
+
+/**/
+
+sudo apt-get update
+
+
+sudo apt-get install tcsh
+
+
+sudo apt-get upgrade
+sudo apt-get remove tcsh
+
+//sudo apt-cache search keyword
+
+---
+
--- rpm.sh
+++ rpm.sh
@@ -1,1 +1,22 @@
 //rpm 
+//
+
+/**/
+
+df /media
+pwd
+
+rpm -ivh ftp-0.17-51.1.el6.i686.rpm
+
+
+rpm -U uppackage.rpm
+
+
+rpm -e oldpackage.rpm
+
+
+rpm -q ftp
+
+
+---
+
