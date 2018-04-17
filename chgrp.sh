a5d7038a22ea8cf4507dfd6d8135bfc50c105ca8
takagotch <dyaccb@gmail.com>
2018-04-18 02:15:32 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- chgrp.sh
+++ chgrp.sh
@@ -9,17 +9,25 @@ tky@DESKTOP-BUM395L MINGW64
 
 /var/tmp/.
 
+
+
+
 id
 ls -l
+
+
 //
+ls -l
 chgrp tky test.text
+
 //
 id
 sudo -s
+pass
 id
 chgrp operator test.text
 ls -l test.text
 
 
-
+---
 
