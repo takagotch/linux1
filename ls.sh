c837f2d0243ea1eb45d73c78b19848a6f5a648d6
takagotch <dyaccb@gmail.com>
2018-04-17 22:22:04 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- ls.sh
+++ ls.sh
@@ -1,5 +1,5 @@
 //ls
-//
+//permission access 
 
 /**/
 
@@ -10,7 +10,25 @@ ls -1
 ls -F /var/spool/
 ls -F bin/*sh
 
+ls -a
+ls -A
 
+//drwxr-xr-x
+
+ls -lF
+
+cd /var/spool
+ls
+ls -t
+ls -lt
+
+ls -ltr
+
+ls -d r*
+ls -dX r*
+
+cd /etc
+ls -R kernel
 
 ---
 
