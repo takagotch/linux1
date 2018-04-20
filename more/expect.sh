1c97ca0e194d987e1fa0b77c1a1960ff203e1307
takagotch <dyaccb@gmail.com>
2018-04-21 03:21:08 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- more/expect.sh
+++ more/expect.sh
@@ -1,1 +1,23 @@
 //expect
+//
+
+cat ftplogin.sh
+/*
+#/bin/sh
+expect -c "
+set timeout 10
+spawn ftp ftp.kddilabs.jp
+expect \):\ ; send anonymous\r
+expect assword:\ ; send ftp\r
+expect ftp>\ ; send \"ls\r\"
+expect fpt>\ ; send \"bye\r\"
+*/
+
+sh ./ftplogin.sh
+usr
+pass
+ls
+
+
+---
+
--- more/make.sh
+++ more/make.sh
@@ -1,1 +1,50 @@
 //make
+
+
+//
+
+
+/* Makefile
+clean:
+	rm *~ a.out
+build: clean
+	cc -o gikope gikope.c
+
+*/
+
+cat Makefile
+#
+# Makefile
+#
+prefix=/usr
+bindir=${prefix}/bin
+RM = /bin/rm
+CC = /usr/bin/cc
+INSTALL = /usr/bin/install
+
+all: clean build install
+
+clean: 
+	${RM} -f a.out *~hello
+build:
+	${CC} -o hello hello.c
+install:
+	${INSTALL} hello ${buildir}
+
+
+make
+
+make clean
+make -s clean
+
+
+sudo make CC=/usr/bin/gcc-4.9
+
+cat Makefile
+export NUMBER=03
+set | grep ^NUMBER
+make
+make -e
+
+---
+
