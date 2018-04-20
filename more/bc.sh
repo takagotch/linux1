de787be9383ddcb1adae28f98209fb98abecb046
takagotch <dyaccb@gmail.com>
2018-04-21 03:28:39 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- more/bc.sh
+++ more/bc.sh
@@ -1,1 +1,14 @@
 //bc
+//let
+
+//
+
+echo "1+2+3" | bc
+
+bc -q
+
+cat tax.txt
+bc -q tax.txt
+
+---
+
--- more/od.sh
+++ more/od.sh
@@ -1,1 +1,15 @@
 //od
+//
+
+//
+
+cat test01.txt
+od -txlc -A n test 01.txt
+
+echo 'tky' | od -x
+
+od /bin/ls -x | head -n 5
+od /bin/ls -x -j 16 -N 16 | head -n 5
+
+---
+
