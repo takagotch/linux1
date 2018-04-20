c2c7fe0bf33dcf3bcee88edfe96c7e5b15c56b6b
takagotch <dyaccb@gmail.com>
2018-04-21 00:24:47 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- more/sudo.sh
+++ more/sudo.sh
@@ -3,6 +3,21 @@
 
 //
 
+sudo til -f /var/log/syslog
+
+
+sudo -l
+pass
+sudo -l
+
+
+
+sudo -u rino touch /home/rino/hoge
+sudo rm /home/rino/hoge
+
+sudo sh -c "cd /home: du -hs > /root/home-du.txt"
+
+
 
 ---
 
