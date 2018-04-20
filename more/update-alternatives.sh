bdc5150dec5174bd5f6ff343f654c43aa4706001
takagotch <dyaccb@gmail.com>
2018-04-21 00:48:05 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- more/update-alternatives.sh
+++ more/update-alternatives.sh
@@ -1,1 +1,23 @@
 //update-alternatives
+//
+
+//
+
+/usr/sbin/update-alternatives --install /usr/bin/editor editor /usr/bin/vim 0
+readlink /usr/bin/editor
+
+readlink /etc/alternatives/editor
+
+/usr/sbin/alternatives --diplay editor
+
+/usr/sbin/alternatives --install /usr/bin/ediotr editor /usr/bin/nano 20
+/usr/sbin/alternatives --display editor
+
+/usr/sbin/alternatives --set editor /usr/bin/vim
+/usr/sbin/alternatives --display editor
+
+/usr/sbin/slateternatives --remove editor /usr/bin/vim
+/usr/sbin/alternatives --display editor
+
+---
+
