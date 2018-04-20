72238927461819d8ffa3686a8062bb359f963e67
takagotch <dyaccb@gmail.com>
2018-04-21 00:18:18 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- more/who.sh
+++ more/who.sh
@@ -1,1 +1,34 @@
-who
+//who
+//w
+
+//
+
+who -H
+
+who -q
+
+who -r
+
+who -m
+who am i
+who are you
+who loves mon
+who mon likes whoa b
+
+
+tty 
+who -T
+write tky pts/4
+
+tty
+
+
+
+
+
+
+
+
+
+---
+
