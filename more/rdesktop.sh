6a3e62cb4d246d6250952c36a65c57aa8d1c7c12
takagotch <dyaccb@gmail.com>
2018-04-21 03:10:56 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- more/rdesktop.sh
+++ more/rdesktop.sh
@@ -1,1 +1,11 @@
 //rdesktop
+//vncviewer
+
+//
+
+rdesktop -g 1902x1080 -k ja 10.10.0.101
+
+
+
+---
+
--- more/tmux.sh
+++ more/tmux.sh
@@ -1,5 +1,19 @@
 //tumx
+//screen
+
+//
+
+tmux ls
+tmux a -t 9
+
+tmux ls
+tmux kill-session -t 9
+tmux ls
+
+tmux new-session \; split-window
 
 
 
+
+---
 
