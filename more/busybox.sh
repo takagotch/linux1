12e89f02262af9d108b143448551af1195136bf9
takagotch <dyaccb@gmail.com>
2018-04-21 00:50:02 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- more/busybox.sh
+++ more/busybox.sh
@@ -1,1 +1,18 @@
 //busybox
+//
+
+//
+
+busybox --list
+
+busybox ls /home
+busybox find /home -maxdepth 2 -mindepth 2 -type f
+
+busybox tar tvfz /usr/share/doc/apg/php.tar.gz | head -n 5
+
+sudo busybox ping -c 3 192.168.11.1
+
+sudo busybox wget http://www.google.jp/
+
+---
+
