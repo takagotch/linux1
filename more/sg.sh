8311f75e122869926767f94e56fd31ecdeb9c321
takagotch <dyaccb@gmail.com>
2018-04-21 00:28:09 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- more/sg.sh
+++ more/sg.sh
@@ -1,1 +1,19 @@
 //sg
+//sudo
+
+//
+
+
+sudo gpasswd newgrp
+pass
+pass
+sg = newgrp -c "touch /tmp/newgrp.txt"
+ls -l /tmp/newgrp.txt
+
+
+sudo sg newgrp -c "touch /tmp/newgrp-02.txt"
+ls -l /tmp/newgrp-02.txt
+
+
+---
+
--- more/sysctl.sh
+++ more/sysctl.sh
@@ -1,1 +1,19 @@
 //sysctl
+//
+
+//
+
+sysctl -a
+sysctl net.ipv6.conf.ech1.accept_ra
+
+sysctl -w net.ipv6.conf.eth1.accept_ra=1
+sysctl net.ipv6.conf.eth1.accept_ra
+
+sysctl -a -r net.ipv6.conf.all.\(accept_ra$\|forwarding\)
+
+// /etc/sysctl.conf
+cat /tmp/new-sysctl.conf
+sysctl -p /tmp/a -r net.ipv6.conf.all.forwarding
+
+---
+
