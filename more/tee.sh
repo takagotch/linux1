309ef7c2e6a4b07e2f9dc3494424c69207a052f6
takagotch <dyaccb@gmail.com>
2018-04-21 01:16:54 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- more/tee.sh
+++ more/tee.sh
@@ -1,1 +1,14 @@
 //tee
+//echo
+
+//
+
+make | tee /tmp/a.log
+lv /tmp/a.log
+
+// /tmp/ls.txt, strace, /tmp/ls-tee.log
+strace -F -f -o /tmp/ls.txt /bin/ls | tee /tmp/ls-tee.log
+./install.sh 2=&1 | tee /tmp/install.log
+
+---
+
