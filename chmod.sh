1730e834fdf7d51187d1015909627c2f85f2cc65
takagotch <30462722+takagotch@users.noreply.github.com>
2019-11-01 10:55:31 +0900

    Update chmod.sh

--- chmod.sh
+++ chmod.sh
@@ -26,6 +26,17 @@ chmod o-w logfile
 ls -l
 
 
-
+# chmod +x /usr/local/bin/docker-compose
+touch test.txt
+ls - l test.txt
+chomd 400 test.txt
+ls -l test.txt
+vi adduser.sh
+chomd 755 adduser.sh
+ls -l adduser.sh
+chomd +x adduser.sh
+./adduser.sh
+chomd g+rx /usr/lib/cgi-bin/test.cgi
+chomd g+r -R /var/www
 ---
 
